4a3e88043d8f95d04d5633fb7451e18a790b5a21
Yassine <yhamidi@iut.univ-paris8.fr>
2022-12-26 15:09:22 +0100

    recuperation de l'id de la div en question et suppresion de la div seulement dans la vue

--- src/Script_js/pageExo.js
+++ src/Script_js/pageExo.js
@@ -36,9 +36,9 @@ $(function() {
             let htmlNouvelExercice
             const uuid = CreateUUID()
             if (classes.includes("exoVraiFaux")) {
-                htmlNouvelExercice = '<div class ="divVraiOuFaux classeDeBase" id="idDivVraiFaux"><input type="text" name="VouF" class="inputVraiF all" /> <button class = "supprimer" onClick="supprimerExo()">❌</button> <p class="pVraiFaux">---------------Vrai----Faux</p> </div>'
+                htmlNouvelExercice = '<div class ="divVraiOuFaux classeDeBase" id="idDivVraiFaux"><input type="text" name="VouF" class="inputVraiF all" /> <button class = "supprimer" onClick="supprimerExo(this)">❌</button> <p class="pVraiFaux">---------------Vrai----Faux</p> </div>'
             } else if (classes.includes("exoAutre")) {
-                htmlNouvelExercice = '<div class ="divVraiOuFaux classeDeBase" id="idDivVraiFaux"><input type="text" name="VouF" class="inputVraiF all" /><button class = "supprimer" onClick="supprimerExo()">❌</button><p class="pVraiFaux">-autre type</p> </div>'
+                htmlNouvelExercice = '<div class ="divVraiOuFaux classeDeBase" id="idDivVraiFaux"><input type="text" name="VouF" class="inputVraiF all" /><button class = "supprimer" onClick="supprimerExo(this)">❌</button><p class="pVraiFaux">-autre type</p> </div>'
             } else if (classes.includes("consigne")) {
                 htmlNouvelExercice = '<div class ="divVraiOuFaux classeDeBase" id="idDivVraiFaux"><input type="text" name="VouF" class="rond all" maxlength="1"/><input type="text" name="VouF" class="consigne2 all" /></div>'
 
--- src/Script_js/supprimerExo.js
+++ src/Script_js/supprimerExo.js
@@ -1,4 +1,18 @@
-function supprimerExo() {
+function supprimerExo(elem) {
+
+    const idAsupp = elem.parentNode.id;
+    console.log(elem.parentNode.id);
+
+
+    elem.parentNode.removeChild(elem);
+
+
+    $.ajax({
+        method: "POST",
+        url: "./modules/mod_editionExo/saveExo.php",
+        data: { divASupp: json },
+        dataType: "json"
+    })
 
 
 }
