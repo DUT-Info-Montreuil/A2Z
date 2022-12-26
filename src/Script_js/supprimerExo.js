cc00f9c44a448b3c794e6946fcbe459022ebabed
Yassine <yhamidi@iut.univ-paris8.fr>
2022-12-26 17:57:47 +0100

    supression quasi finit, fonctionne bien dans la bdd mais il faut que ca supprime celui que l'on à selectionner

--- src/Script_js/supprimerExo.js
+++ src/Script_js/supprimerExo.js
@@ -1,28 +1,31 @@
+/**
+ * fonction qui permet de supprimer un exo en le supprimant dans la vue puis en envoyant en ajax pour le supprimer dans la bdd
+ * @param {*} elem correspond au this de onClick="supprimerExo(this)" qui est l'exo actuel
+ */
 function supprimerExo(elem) {
 
 
     var element = document.getElementById(elem.parentNode.id);
     console.log(element.id)
     console.log(elem);
-    elem.parentNode.remove(element);
+
+    let elementId = element.id;
+
+    console.log(elementId);
+
+
 
     /*
-        let idExoAsupp = elem.parentNode.id;
-        console.log(idExoAsupp)
-
-
-        let idExoAsuppjson = JSON.stringify(idExoAsupp);
-        console.log(idExoAsuppjson)
-
-        var t = document.getElementById(elem.parentNode.id);
-        elem.parentNode.removeChild(t.parentNode.id);
-
 
         $.ajax({
             method: "POST",
             url: "./modules/mod_editionExo/saveExo.php",
-            data: { idAsupp: idExoAsuppjson },
+            data: { idAsupp: elementId },
             dataType: "json"
-        })*/
+        })
+    */
+
+    elem.parentNode.remove(element);
+
 
 }
--- src/modules/mod_editionExo/saveExo.php
+++ src/modules/mod_editionExo/saveExo.php
@@ -21,14 +21,16 @@ class saveExo extends connexion
         $tableauContenuExerciceDecode = json_decode($exerciceJSON, true);
         //decoder le json en fichier
 
+        /*
         $idAsupprimer = $_POST['idAsupp']; // tableau en JSON contenant tout (id et le code html)
+        var_dump($idAsupprimer);
+        */
 
-        echo $idAsupprimer;
+
+        
         try {
             // faire une boucle for qui va d'une part
             $idFiche = htmlspecialchars($tableauContenuExerciceDecode['idFiche']);
-
-
 
 
 
@@ -36,15 +38,52 @@ class saveExo extends connexion
             $sql = 'INSERT into exercices (idExo, contenu, idFiche)  VALUES (:idExo , :contenu, :idFiche)';
             $sql1 = 'Select * from exercices where idFiche = :idFiche and idExo = :idExo'; // selectionenr les exercices avec lequel l'id de la fiche existe deja et si l'exo existe deja 
             $sql2 = 'UPDATE exercices SET contenu= :contenu WHERE idExo =:idExo';
-
+            $sql3 = 'Select idExo from exercices where idFiche = :idFiche'; // selectionenr les exercices avec lequel l'id de la fiche existe deja et si l'exo existe deja 
+            $sql4 = 'DELETE FROM exercices WHERE idExo =:idExo';
 
             //preparation des requetes SQL
             $statement = Connexion::$bdd->prepare($sql);
             $statement1 = Connexion::$bdd->prepare($sql1);
             $statement2 = Connexion::$bdd->prepare($sql2);
+            $statement3 = Connexion::$bdd->prepare($sql3);
+            $statement4 = Connexion::$bdd->prepare($sql4);
 
 
-            for ($i = 0; $i < count($tableauContenuExerciceDecode['idExo']); $i++) { //verification de tout les exos
+
+            $statement3->execute(array(':idFiche' => $idFiche));
+            $exoDansLaBDD = $statement3->fetchAll();
+            var_dump(count($exoDansLaBDD));
+
+            
+            //                count = 3
+            for($i = 0 ; $i < count($exoDansLaBDD); $i++){  //parcours de la bd 
+                
+                echo count($tableauContenuExerciceDecode['idExo']);
+                var_dump(count($exoDansLaBDD));
+                $idExercice = htmlspecialchars($tableauContenuExerciceDecode['idExo'][$i]); //recuperation de l'id de l'exo
+                $html =  htmlspecialchars($tableauContenuExerciceDecode['html'][$i]); // recuperation de l'html
+
+                var_dump($idExercice);
+                var_dump($html);
+                echo $i;
+
+                if(in_array($tableauContenuExerciceDecode['idExo'],$exoDansLaBDD)){//update
+                    $tableauExec = array(':contenu' => json_encode($html), ':idExo' => $idExercice);
+                    $statement2->execute($tableauExec);
+                    echo 'update';
+                    
+                }
+                else{
+                    $tableauExec = array(':idExo' => $idExercice);//delete
+                    echo $idExercice;
+                    $statement4->execute($tableauExec);
+                    echo 'je suis la ';
+                    
+                }
+            }
+
+
+            /*for ($i = 0; $i < count($tableauContenuExerciceDecode['idExo']); $i++) { //verification de tout les exos
 
                 //variable de recuperation de données
                 $idExercice = htmlspecialchars($tableauContenuExerciceDecode['idExo'][$i]); //recuperation de l'id de l'exo
@@ -65,7 +104,12 @@ class saveExo extends connexion
                     $statement->execute($tableauExec); //vois si pour le mdp on fait htmlspecialchars
                     
                 }
-            }
+            }*/
+
+           
+            
+
+
         } catch (PDOException $e) {
             echo $e->getMessage() . $e->getCode();
         }
